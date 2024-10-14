6b1c9862228e5b0f0df399e4be42764e9df4dab9
bvchnguyen <bang4509@colorado.edu>
2024-10-14 16:29:59 -0400

    Setup for API call

---
+++ week1/SoftwareSolution/Software.Api/Catalog/CatalogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Software.Api.Catalog;
+
+public class CatalogController : ControllerBase
+{
+    [HttpPost("/Catalog")]
+    public async Task<ActionResult> AddSoftwareCatalogAsync()
+    {
+        return Ok();
+    }
+}
